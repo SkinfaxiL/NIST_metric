4173561e1a0092935c22b1b015476979eb6b9193
SkinfaxiL <tobylee@Tobys-MacBook-Pro.local>
2021-01-05 15:45:27 -0500

    modify compare_script

--- compare_script.sh
+++ compare_script.sh
@@ -3,10 +3,6 @@
 nohup python3 compare.py --dp_data ./data/Event/privatized-mediocre-quality.csv &
 nohup python3 compare.py --dp_data ./data/Event/privatized-very-poor-quality.csv &
 
-## sprint1 submission
-#nohup python3 compare.py --dp_data ./data/sprint1/eps1.csv &
-#nohup python3 compare.py --dp_data ./data/sprint1/eps2.csv &
-#nohup python3 compare.py --dp_data ./data/sprint1/eps10.csv &
 
 # sprint1 laplace
 nohup python3 compare.py --dp_data ./data/sprint1/lap_eps0.1.csv &
@@ -20,14 +16,13 @@ nohup python3 compare.py --dp_data ./data/sprint1/lap_eps10.csv &
 
 # DP syn data scores
 
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-0.1-2.pkl --is_dp_data_pickle True &
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-0.25-2.pkl --is_dp_data_pickle True &
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-0.5-2.pkl --is_dp_data_pickle True &
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-1.0-2.pkl --is_dp_data_pickle True &
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-2.0-2.pkl --is_dp_data_pickle True &
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-4.0-2.pkl --is_dp_data_pickle True &
-nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-10.0-9.pkl --is_dp_data_pickle True &
-#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-100-20.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-0.1-2.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-0.25-2.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-0.5-2.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-1.0-2.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-2.0-2.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-4.0-2.pkl --is_dp_data_pickle True &
+#nohup python3 compare.py --dp_data ./data/event_dpsyn/2019-dpsyn-10.0-9.pkl --is_dp_data_pickle True &
 
 # sprint1 sampled
 nohup python3 compare.py --dp_data ./data/sprint1/0.01_sampling.csv &
@@ -35,4 +30,3 @@ nohup python3 compare.py --dp_data ./data/sprint1/0.05_sampling.csv &
 nohup python3 compare.py --dp_data ./data/sprint1/0.1_sampling.csv &
 nohup python3 compare.py --dp_data ./data/sprint1/0.25_sampling.csv &
 nohup python3 compare.py --dp_data ./data/sprint1/0.5_sampling.csv &
-#nohup python3 compare.py --dp_data ./data/sprint1/0.8_sampling.csv &
